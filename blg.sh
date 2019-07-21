fca54dffa38e7d757477b6f5cb66f73e303c67b4
vcheckzen <vcheckzen@outlook.com>
2019-07-21 17:25:01 +0800

    chore[all]: regular update

--- blg.sh
+++ blg.sh
@@ -143,14 +143,16 @@ case $1 in
     'upgrade'|8) upgrade
     ;;
     *)
-        echo "欢迎使用 Blog 助手，您可执行以下命令！"
-        echo "blg 1 | init           - 安装必要软件"
-        echo "blg 2 | set            - 设置 Github 账号"
-        echo "blg 3 | key            - 生成 SSH 密钥"
-        echo "blg 4 | update         - 拉取 Blog 源码"
-        echo "blg 5 | new <title>    - 生成新文章"
-        echo "blg 6 | preview        - 本地预览"
-        echo "blg 7 | deploy         - 发布到 Github"
-        echo "blg 8 | upgrade        - 更新 Blog 助手"
+    : <<EOF 
+欢迎使用 Blog 助手，您可执行以下命令！
+blg 1 | init           - 安装必要软件
+blg 2 | set            - 设置 Github 账号
+blg 3 | key            - 生成 SSH 密钥
+blg 4 | update         - 拉取 Blog 源码
+blg 5 | new <title>    - 生成新文章
+blg 6 | preview        - 本地预览
+blg 7 | deploy         - 发布到 Github
+blg 8 | upgrade        - 更新 Blog 助手
+EOF
 esac
 
