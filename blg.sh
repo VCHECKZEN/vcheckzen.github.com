2ff3c587d3e678d8670e03370abce3a95fb75fce
vcheckzen <vcheckzen@outlook.com>
2019-07-21 12:47:13 +0800

    chore[all]: add source

--- blg.sh
+++ blg.sh
@@ -2,7 +2,6 @@
 function init()
 {
     echo "开始初始化..."
-    sed -i 's@^\(deb.*stable main\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux stable main@' $PREFIX/etc/apt/sources.list
     apt update && apt upgrade -y
     apt install termux-tools termux-api -y
     termux-setup-storage
