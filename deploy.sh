f8e4147b0b8bf138d8ae67520bfd3b01bc3fdb06
vcheckzen <vcheckzen@outlook.com>
2019-08-12 02:48:19 +0800

    feat(article): write a new article

--- deploy.sh
+++ deploy.sh
@@ -2,22 +2,22 @@
 
 SOURCES="/home/logi/git/github/hexo/"
 DEPLOY="/home/logi/git/github/deploy.sh"
-GITHUB_PAGES="/home/logi/git/github/vcheckzen.github.com"
-GITHUB_PAGES_BACKUP="/home/logi/git/github/vcheckzen.github.com.bak"
+GITHUB_PAGES="/home/logi/git/github/vcheckzen.github.com/"
+GITHUB_PAGES_BACKUP="/home/logi/git/github/vcheckzen.github.com.bak/"
 
 # 推送编译后文件
-cd $SOURCES
+cd "${SOURCES}"
 hexo clean
 hexo g
 
-cd $GITHUB_PAGES
+cd "${GITHUB_PAGES}"
 git checkout master
 rm -rf *
-cp -r "$SOURCES/public/"* $GITHUB_PAGES
-cp "$GITHUB_PAGES_BACKUP/404.html" $GITHUB_PAGES
-cp "$GITHUB_PAGES_BACKUP/favicon".* $GITHUB_PAGES
-cp "$GITHUB_PAGES_BACKUP/robots.txt" $GITHUB_PAGES
-cp -r "$GITHUB_PAGES_BACKUP/customized" $GITHUB_PAGES
+cp -r "${SOURCES}/public/"* "${GITHUB_PAGES}"
+cp "${GITHUB_PAGES_BACKUP}404.html" "${GITHUB_PAGES}"
+cp "${GITHUB_PAGES_BACKUP}favicon".* "${GITHUB_PAGES}"
+cp "${GITHUB_PAGES_BACKUP}robots.txt" "${GITHUB_PAGES}"
+cp -r "${GITHUB_PAGES_BACKUP}customized" "${GITHUB_PAGES}"
 git add -A
 git commit -m "feat(article): write a new article"
 git push -f -u origin master
@@ -25,9 +25,9 @@ git push -f -u origin master
 # 推送源文件
 git checkout source
 rm -rf *
-cp $DEPLOY ./
-cp -r $SOURCES ./
-cp -r $GITHUB_PAGES_BACKUP ./
+cp "${DEPLOY}" ./
+cp -r "${SOURCES}" ./
+cp -r "${GITHUB_PAGES_BACKUP}" ./
 git add -A
 git commit -m "feat(article): write a new article"
 git push -f -u origin source
